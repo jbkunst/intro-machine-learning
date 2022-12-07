55fd89b6726d16cbf6dabe692137425ea2e0f926
Joshua Kunst <jbkunst@gmail.com>
2022-12-07 14:40:03 -0300

    Create 29221207-regresion-aplicada.R

---
+++ R/29221207-regresion-aplicada.R
@@ -0,0 +1,68 @@
+library(tidyverse)
+library(lubridate)
+library(equatiomatic)
+library(latex2exp)
+
+datos <- read_csv("data/bike_day.csv")
+
+glimpse(datos)
+
+ggplot(datos) +
+    geom_point(aes(dteday, cnt))
+
+datos <- datos |> 
+  mutate(muestra = ifelse(dteday < ymd(20120701), "train", "test"))
+
+ggplot(datos) +
+  geom_point(aes(dteday, cnt, color = muestra))
+
+
+
+fit_lm_bike <- function(fmla = cnt ~ yr) {
+  
+  dentrenaimento <-  datos |> 
+    filter(muestra == "train") |> 
+    select(-muestra)
+  
+  m <- lm(formula = fmla, data = dentrenaimento)
+  
+  
+  # extract equation with `ital_vars = TRUE` to avoid the use of `\operatorname`
+  m_eq <- extract_eq(m, use_coef = TRUE, ital_vars = TRUE)
+  # swap escaped underscores for dashes
+  prep_eq <- gsub("\\\\_", "-", m_eq)
+  # swap display-style $$ with inline-style $
+  prep_eq <- paste("$", as.character(prep_eq), "$", sep = "")
+
+  
+  dplot <- datos |> 
+    mutate(pred = predict(m, datos)) |> 
+    select(dteday, cnt, pred, muestra)
+  
+  ggplot(dplot) +
+    geom_point(aes(dteday, cnt, color = muestra)) +
+    geom_line(aes(dteday, pred), color = "purple") +
+    scale_color_viridis_d(begin = 0.1, end = 0.9) +
+    scale_y_continuous(labels = scales::comma) +
+    labs(
+      color = "",
+      caption = latex2exp::TeX(prep_eq)
+      ) +
+    
+    theme_minimal() +
+    theme(legend.position =  "bottom")
+  
+}
+
+
+fit_lm_bike(cnt ~ yr)
+
+fit_lm_bike(cnt ~ yr + mnth + season)
+
+fit_lm_bike(cnt ~ yr + mnth + season + instant)
+
+
+
+
+
+           
